d72e6ae40bbed05c0195db850324f7e0bb715c63
jacksomeone-92 <49621739+jacksomeone-92@users.noreply.github.com>
2020-07-02 21:49:14 +0200

    Update Human Behaviour Prediction Game.py
    Status based on video from prof

--- Statistical Programming - Python/Human Behaviour Prediction Game.py
+++ Statistical Programming - Python/Human Behaviour Prediction Game.py
@@ -2,8 +2,51 @@
 """
 Created on Thu Jul  2 21:01:04 2020
 
-@author: pleaz
+@author: Nicolas G
 """
 
-#test connection to github
-print("something")
+#define the function for linear congruences
+def linear_congruence(xi):
+    """ Function to calculate linear congruences value and computer bet """
+    a = 22695477
+    b = 1
+    m = 2**32
+    xi_plus_1 = (a * xi + b) % m
+    if xi_plus_1 <= 2**31:
+        comp_move = 0
+    else: 
+        comp_move = 1
+    return comp_move, xi_plus_1
+
+#1. Create an opening message
+print("Welcome to Human Behavior Prediction by Prof. Manoel Gadi")
+
+select_difficulty = int(input("Choose the type of game (1: Easy; 2: Difficult): "))
+
+moves = int(input("Enter the number of moves: "))
+
+MS = 0
+PS = 0
+xi = 1234
+
+if select_difficulty == 1:
+    for turn in range(moves):
+        #print(turn)
+        player_move = int(input("Choose your move number %s (0 or 1):" % (turn+1)))
+        computer_move,xi = linear_congruence(xi)
+        if player_move == computer_move:
+            MS = MS + 1
+            print("player = 0 machine = 0 - Machine wins!")
+            print("You: %d Computer: %d" % (PS, MS))
+        else:
+            PS = PS + 1
+            print("player = 0 machine = 0 - Machine wins!")
+            print("You: %d Computer: %d" % (PS, MS))
+            
+        print('PLAYER: ' + '*'*PS)
+        print('MACHINE: ' + '*'*MS)
+            
+# Open:
+# create overall for loop that only ends when the play decides to quit the game
+# implement difficulty 2
+                
